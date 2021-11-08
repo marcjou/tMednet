b036fdad0a9c369a09d9c8e058d0f1e8a49da76e
marc <marcjb2@gmail.com>
2021-11-08 13:24:09 +0100

    Processed CPN, CPS, ULLASTRES

--- script.py
+++ script.py
@@ -1,12 +1,12 @@
 import re
 
 
-f = open('/home/marc/PycharmProjects/MedNet/src/Medes/masarchivos/6_20210724-12_20211019-11_05.txt', "r")
+f = open('/home/marc/PycharmProjects/MedNet/src/ullastres/150_20210720-13_20211010-11_05.txt', "r")
 good = f.readlines()
 good[:] = map(lambda item: re.sub('\t+', ' ', item.strip()).split(' '), good)
 f.close()
 
-f = open('/home/marc/PycharmProjects/MedNet/src/Medes/masarchivos/6_20210724-12_20211019-11_35.txt', "r")
+f = open('/home/marc/PycharmProjects/MedNet/src/ullastres/150_20210720-13_20211010-11_30.txt', "r")
 tobeChanged = f.readlines()
 tobeChanged[:] = map(lambda item: re.sub('\t+', ' ', item.strip()).split(' '), tobeChanged)
 f.close()
@@ -16,7 +16,7 @@ for i in range(len(good)):
         tobeChanged[i][1] = good[i][1]
         tobeChanged[i][2] = good[i][2]
 
-with open("/home/marc/PycharmProjects/MedNet/src/Medes/6_20210724-12_20211019-11_35.txt", "w") as txt_file:
+with open("/home/marc/PycharmProjects/MedNet/src/ullastres/150_20210720-13_20211010-11_30.txt", "w") as txt_file:
     for line in tobeChanged:
         txt_file.write(" ".join(line) + "\n")
 
