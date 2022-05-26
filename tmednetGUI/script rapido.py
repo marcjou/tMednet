a119e2e0552a711c1dbf15db131ed0a295bcb6ac
marc <marcjb2@gmail.com>
2022-05-26 16:10:07 +0200

    Updated the additional script to convert tobs to database. Now it has been optimized to less lines. Efficency improved more than 110 times.

--- tmednetGUI/script rapido.py
+++ tmednetGUI/script rapido.py
@@ -3,26 +3,37 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import progressbar as pb
-
+import time
+start = time.time()
 file = pd.read_csv('../src/Tobs_05_Cap de Creus-S_200705-202110.txt', sep='\t')
 file2 = file.rename(columns={'NaN': 'Date', 'NaN.1': 'Time'})
 datime = []
 nanline = []
 i = 0
-progress_bar = pb.progressBar(len(file2['Date']), prefix='Progress:', suffix='Complete', length=50)
 
+#progress_bar = pb.progressBar(len(file2['Date']), prefix='Progress:', suffix='Complete', length=50)
+
+# TESTS
+file2.dropna(how='all', inplace=True) #Drops all the lines that are pure NaN
+file2['Date'] = file2['Date'].astype(int)
+file2['Date'] = file2['Date'].map(lambda x: datetime.strftime(datetime.fromordinal(datetime(1900, 1, 1).toordinal() + int(x) - 2), '%d/%m/%Y'))
+file2['Time'] = file2['Time'].map(lambda x: str(int(float(x) * 24)) + ':0' + str(int(((float(x) * 24) % 1) * 60)) + ':0' + str(int(((((float(x) * 24) % 1) * 60) % 1)* 60)) if len(str(int(((float(x) * 24) % 1) * 60))) == 1 else str(int(float(x) * 24) + 1) + ':00:00')
+file2['Time'] = file2['Time'].map(lambda x: '0' + x if len(x) == 7 else x)
+
+'''
+# Not necessary with what we have above much faster
 for x in file2['Date']:
     if math.isnan(x) == False:
         file2['Date'][i] = datetime.strftime(datetime.fromordinal(datetime(1900, 1, 1).toordinal() + int(x) - 2), '%d/%m/%Y')
     else:
         nanline.append(i)
-    progress_bar.print_progress_bar(i)
+    #progress_bar.print_progress_bar(i)
     i = i + 1
 for n in nanline:
     file2.drop(index=n, inplace=True)
 i = 0
+#progress_bar = pb.progressBar(len(file2['Time']), prefix='Progress:', suffix='Complete', length=50)
 
-progress_bar = pb.progressBar(len(file2['Time']), prefix='Progress:', suffix='Complete', length=50)
 
 for x in file2['Time']:
     try:
@@ -45,15 +56,23 @@ for x in file2['Time']:
         file2['Time'][i] = time
     except ValueError:
         file2.drop(index=i, inplace=True)
-    progress_bar.print_progress_bar(i)
+    #progress_bar.print_progress_bar(i)
     i = i + 1
 
 file2['added'] = file2['Date'].astype(str) + ' ' + file2['Time'].astype(str)
 
+end = time.time()
+print(end-start)
 
 file2.set_index('added', inplace=True)
 file2.index.name = None
 del file2['Date']
 del file2['Time']
+end = time.time()
+print(end-start)
+'''
 
-file2.to_csv('../src/output_files/CPS_Try.txt', sep='\t', index=True)
+file2.to_csv('../src/output_files/CPS_Try_Old.txt', sep='\t', index=False)
+
+end = time.time()
+print(end-start)
