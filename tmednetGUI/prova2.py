5a5c6b0095b27f6cad4e7adad696bef7d288e197
marc <marcjb2@gmail.com>
2022-06-22 15:33:28 +0200

    Improved the code for mapping, added first steps to calculate mhw. Modified Oliver's code to accept a previous Climatology. FInd the climatology (Nathaniel) and load it to pass it to the code.

--- tmednetGUI/prova2.py
+++ tmednetGUI/prova2.py
@@ -16,7 +16,41 @@ from mpl_toolkits.basemap import Basemap
 import imageio
 from datetime import datetime
 
-nc = NetCDFFile('../src/input_files/SST_MED_SST_L4_NRT_OBSERVATIONS_010_004_a_V2_1655810288836.nc')
+
+def map_temperature(lat, lon, realtime, asst):
+    map = Basemap(projection='merc', llcrnrlon=-9.5, llcrnrlat=28., urcrnrlon=37., urcrnrlat=50., resolution='i')
+    map.drawcoastlines(linewidth=0.5)
+    map.drawcountries()
+    map.drawlsmask(land_color='#adc6a9', ocean_color='#608abf')  # can use HTML names or codes for colors
+    lons, lats = np.meshgrid(lon, lat)
+    x, y = map(lons, lats)
+    filenames = []
+    levels = np.arange(math.trunc(float(np.amin(asst)) - 273.15), math.trunc(float(np.amax(asst)) - 273.15) + 1, 1)
+
+    # mhw.detect(ordtime, temp)
+
+    for i in range(0, asst.shape[0]):
+        temp = map.contourf(x, y, asst[i, :, :] - 273.15, cmap='RdYlBu_r')
+        if i == 0:
+            cb = map.colorbar(temp, "bottom", size="5%", pad="2%", ticks=levels)
+        plt.title(realtime[i])
+        print('hey')
+        plt.savefig('../src/output_images/image_' + str(i) + '.png')
+        print('hoy')
+        filenames.append('../src/output_images/image_' + str(i) + '.png')
+
+    # build gif
+    with imageio.get_writer('mygif.gif', mode='I', duration=0.7) as writer:
+        for filename in filenames:
+            image = imageio.imread(filename)
+            writer.append_data(image)
+    import os
+    # Remove files
+    for filename in set(filenames):
+        os.remove(filename)
+
+
+nc = NetCDFFile('../src/input_files/SST_MED_SST_L4_NRT_OBSERVATIONS_010_004_c_V2_1655884625217.nc')
 lat = nc.variables['lat'][:]
 lon = nc.variables['lon'][:]
 time = nc.variables['time'][:]
@@ -27,36 +61,16 @@ realtime = [i.strftime('%Y-%m-%d') for i in time]
 dtime = [datetime.strptime(i, '%Y-%m-%d') for i in realtime]
 ordtime = [x.toordinal() for x in dtime]
 asst = nc.variables['analysed_sst'][:]
-map = Basemap(projection='merc',llcrnrlon=-9.5,llcrnrlat=28.,urcrnrlon=37.,urcrnrlat=50.,resolution='i')
-map.drawcoastlines(linewidth=0.5)
-map.drawcountries()
-map.drawlsmask(land_color='#adc6a9', ocean_color='#608abf') # can use HTML names or codes for colors
-lons, lats = np.meshgrid(lon, lat)
-x, y = map(lons, lats)
-filenames = []
-levels = np.arange(math.trunc(float(np.amin(asst)) - 273.15), math.trunc(float(np.amax(asst)) - 273.15)+ 1, 1)
 
-#mhw.detect(ordtime, temp)
-
-for i in range(0, asst.shape[0]):
-    temp = map.contourf(x,y,asst[i,:,:] - 273.15, cmap='RdYlBu_r')
-    if i == 0:
-        cb = map.colorbar(temp, "bottom", size="5%", pad="2%", ticks=levels)
-    plt.title(realtime[i])
-    print('hey')
-    plt.savefig('../src/output_images/image_' + str(i) + '.png')
-    print('hoy')
-    filenames.append('../src/output_images/image_' + str(i) + '.png')
+for i in range(0,len(lat)):
+    for j in range(0,len(lon)):
+        if np.ma.is_masked(asst[0,i,j]):
+            pass
+        else:
+            sst=asst[:,i,j].tolist()
+            mhws, clim = mhw.detect(ordtime, sst)
 
 
-# build gif
-with imageio.get_writer('mygif.gif', mode='I', duration=0.7) as writer:
-    for filename in filenames:
-        image = imageio.imread(filename)
-        writer.append_data(image)
-import os
-# Remove files
-for filename in set(filenames):
-    os.remove(filename)
+map_temperature(lat, lon, realtime, asst)
 
 print('hola')
