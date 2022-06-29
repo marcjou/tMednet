c8fdfb715832682925f4881398db9c1a3ff5db07
marc <marcjb2@gmail.com>
2022-06-29 19:14:29 +0200

    Version solved

--- tmednetGUI/prova.py
+++ tmednetGUI/prova.py
@@ -16,6 +16,8 @@ import seaborn as sns
 from scipy import io
 mat = io.loadmat('../src/mhwclim_1982-2011_L4REP_MED.mat')
 
+# TODO keep on working on implementing NAT clim to our code. Needed to interpolate his resolution with ours, check how to do it, maybe solved, took too long
+
 
 nc = NetCDFFile('../src/SST_MED_SST_L4_NRT_OBSERVATIONS_010_004_c_V2_1656500346190.nc')
 lat = nc.variables['lat'][:]
@@ -32,6 +34,8 @@ clim_lat = mat['mhwclim']['lat'][0,0][:, 0]
 clim_lon = mat['mhwclim']['lon'][0,0][0, :]
 
 clim_lons, clim_lats = np.meshgrid(clim_lon, clim_lat)  # 2D grid for interpolation
+
+# TODO here I talk about interpolation
 
 interp = LinearNDInterpolator(list(zip(lon, lat)), sst_day1)
 
