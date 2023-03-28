5d0efe455b9b4108d0ab2a61f0858576e1abf9e4
Marc jou <marcjou@ide-uno.cmima.csic.es>
2023-03-28 15:08:52 +0200

    Modified the last shell script to perfectly capture yesterdays month.

--- tmednetGUI/upload_images.sh
+++ tmednetGUI/upload_images.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-# WRITE THE WAY TO SELECT THE CURRENT MONTH TO UPLOAD THE FILES THAT SHOULD BE UPLOADED
+
 sv="t-mednet.org 21"
 user="t-mednet"
 pass="lk@hRTpo1"
+month=$(date --date='yesterday' +'%b')
 
 cd ../src/output_images
 
@@ -12,8 +13,8 @@ quote USER $user
 quote PASS $pass
 
 cd httpdocs/images/heatwaves/2023
-put anim_MHW_days_Jan.gif
-
+put anim_MHW_days_${month}.gif
+put anim_MHW_imax_${month}.gif
 quit 
 
 END_SCRIPT
