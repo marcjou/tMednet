f34c6347d2f5b6e8641fcf74ff1f104cc75a97b5
marc <marcjb2@gmail.com>
2024-11-06 10:02:45 +0100

    Created radar plot with multiple sites. It looks cool.

--- tmednetGUI/proba marti.py
+++ tmednetGUI/proba marti.py
@@ -4,7 +4,10 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 import seasampler as samp
 
-#object = samp.SeaSampler('../src/input_files/SeaSampler', 'control depth', 'control_depth_V2')
-#object = samp.SeaSampler('../src/input_files/SeaSampler', 'metadata', 'metadata_seasampler_V3')
+#object = samp.SeaSampler('../src/input_files/Boreas', 'control depth', 'control_depth_Boreas')
+#object = samp.SeaSampler('../src/input_files/SeaSampler', 'metadata', 'metadata_seasampler_V4')
 object = samp.SeaSampler('../src/input_files/SeaSampler Tossa', 'plots', 'plots')
+object.add_data_list('../src/input_files/SeaSampler Boreas')
+object.add_data_list('../src/input_files/SeaSampler Medes')
+object.add_data_list('../src/input_files/SeaSampler Ullastres')
 object.radar_plot()
--- tmednetGUI/seasampler.py
+++ tmednetGUI/seasampler.py
@@ -5,6 +5,7 @@ import csv
 import re
 import matplotlib.pyplot as plt
 
+
 class SeaSampler():
 
     def __init__(self, path, type, output_file):
@@ -14,7 +15,7 @@ class SeaSampler():
             df = pd.DataFrame(columns=columns)
             # path = '../src/input_files/SeaSampler'
             df, bad_list = self.dir_reader(df, path, self.dict_creator)
-            df.to_excel('../src/output_files/' + output_file +  '.xlsx')
+            df.to_excel('../src/output_files/' + output_file + '.xlsx')
             self.save_txt('bad_entries_' + output_file, bad_list)
 
         if type == 'control depth':
@@ -25,6 +26,15 @@ class SeaSampler():
             self.temp_depth_data = df.sort_values(by='date')
             print('he')
 
+    def add_data_list(self, path):
+        if hasattr(self, 'data_list'):
+            df, bad_list = self.dir_reader(df=pd.DataFrame(), path=path, func=self.mega_dataframe)
+            self.data_list.append(df.sort_values(by='date'))
+        else:
+            self.data_list = []
+            self.data_list.append(self.temp_depth_data)
+            df, bad_list = self.dir_reader(df=pd.DataFrame(), path=path, func=self.mega_dataframe)
+            self.data_list.append(df.sort_values(by='date'))
 
     @staticmethod
     def save_txt(filename, my_list):
@@ -55,45 +65,79 @@ class SeaSampler():
         else:
             raise ValueError("El formato de la coordenada no es válido.")
 
-    def radar_plot(self):
-        # Prepare the data for the plot by rounding the depths to integers and  calculating the mean
-        data = self.temp_depth_data.copy()
-        data['depth(m)'] = data['depth(m)'].round()
-        data_mean = data.groupby(by='depth(m)', as_index=False)['temperature(c)'].mean()
-
+    def prepare_radar_plot(self, labels):
         # Prepare the labels
-        labels = data_mean['depth(m)'].unique()
+
         num_vars = len(labels)
 
         # Crear el gráfico de radar
-        fig, ax = plt.subplots(figsize=(8, 8), subplot_kw=dict(polar=True))
+        fig, ax = plt.subplots(figsize=(12, 8), subplot_kw=dict(polar=True))
 
         # Convertir etiquetas en ángulos para el gráfico de radar
         angles = np.linspace(0, 2 * np.pi, num_vars, endpoint=False).tolist()
         angles += angles[:1]
 
+        return ax, angles
 
-        # Repetir el primer valor al final para cerrar el gráfico
-        data_mean = pd.concat([data_mean, pd.DataFrame([data_mean.iloc[0]])], ignore_index=True)
+    # TODO add the possibility to plot multiple sites
+    def radar_plot(self):
+        if hasattr(self, 'data_list'):
+            dip = 0
+            for data in self.data_list:
+                if data['depth(m)'].max() > dip:
+                    dip = data['depth(m)'].max().round()
+            labels = range(1, int(dip) + 1, 1)
+            ax, angles = self.prepare_radar_plot(labels)
+            color = ['blue', 'red', 'yellow', 'green']
+            site = ['Tossa', 'Boreas', 'Medes', 'Ullastres']
+            i = 0
+            for data in self.data_list:
+                data['depth(m)'] = data['depth(m)'].round()
+                data_mean = data.groupby(by='depth(m)', as_index=False)['temperature(c)'].mean()
+                # First add nan if depth is lower than max
+                if data_mean['depth(m)'].iloc[-1] != dip:
+                    fdepth = data_mean['depth(m)'].iloc[-1]
+                    diff = dip - fdepth
+                    for n in range(1, int(diff) + 1, 1):
+                        new_row = pd.DataFrame({'depth(m)': [fdepth + n], 'temperature(c)': [np.nan]})
+                        data_mean = pd.concat([data_mean, new_row], ignore_index=True)
+                # Repetir el primer valor al final para cerrar el gráfico
+                data_mean = pd.concat([data_mean, pd.DataFrame([data_mean.iloc[0]])], ignore_index=True)
 
+                ax.plot(angles, data_mean['temperature(c)'], color=color[i], linewidth=2,
+                        label='Average temperature - ' + site[i])
 
+                # Rellenar el área bajo la curva
+                ax.fill(angles, data_mean['temperature(c)'], color=color[i], alpha=0.1)
+                i += 1
+        else:
+            # Prepare the data for the plot by rounding the depths to integers and  calculating the mean
+            data = self.temp_depth_data.copy()
+            data['depth(m)'] = data['depth(m)'].round()
+            data_mean = data.groupby(by='depth(m)', as_index=False)['temperature(c)'].mean()
+            labels = data['depth(m)'].unique()
+            ax, angles = self.prepare_radar_plot(labels)
 
-        ax.plot(angles, data_mean['temperature(c)'], color='blue', linewidth=2, label='Average temperature - Tossa')
+            # Repetir el primer valor al final para cerrar el gráfico
+            data_mean = pd.concat([data_mean, pd.DataFrame([data_mean.iloc[0]])], ignore_index=True)
 
-        # Rellenar el área bajo la curva
-        ax.fill(angles, data_mean['temperature(c)'], color='blue', alpha=0.1)
+            ax.plot(angles, data_mean['temperature(c)'], color='blue', linewidth=2, label='Average temperature - Tossa')
+
+            # Rellenar el área bajo la curva
+            ax.fill(angles, data_mean['temperature(c)'], color='blue', alpha=0.1)
 
         # Configuración de etiquetas y leyenda
-        ax.set_yticklabels([])
+        #ax.set_yticklabels(['haha', 'hehe'])
         ax.set_xticks(angles[:-1])
         ax.set_xticklabels(labels, color='skyblue', fontsize=12)
         ax.legend(loc='upper right', bbox_to_anchor=(1.3, 1.1))
-        plt.savefig('../src/output_images/radar_tossa.png')
+        plt.savefig('../src/output_images/radar_combo4_v1.png')
+
     def mega_dataframe(self, file_path, file_name, df, bad_list):
         df = df.append(pd.read_csv(file_path))
         return df, 'bad'
 
-    #TODO convertir date a timestamp o datetime y hacer caso a chatgpt
+    # TODO convertir date a timestamp o datetime y hacer caso a chatgpt
     def plot_depthvstemp(self, filename):
         self.temp_depth_data['date'] = pd.to_datetime(self.temp_depth_data['date'])
         # Probamos un resample para suavizar las lineas
@@ -106,19 +150,20 @@ class SeaSampler():
         ax2.invert_yaxis()
         plt.savefig('../src/output_images/' + filename + '.png')
         print('alo')
-    #TODO work on using *args and **kwargs to make the functions more flexible
+
+    # TODO work on using *args and **kwargs to make the functions more flexible
     def check_depths(self, file_path, file_name, df, bad_list, *args):
-        df = pd.read_csv(file_path , skiprows=16)
+        df = pd.read_csv(file_path, skiprows=16)
         df['depth(m)'] = - df['depth(m)']
         # devuelve las entradas que estan por debajo de un metro de profundidad
         if df.loc[df['depth(m)'] < 1].empty:
             text = 'Depth ok in: ' + file_name
             bad_list.append(text)
-            df.to_csv('../src/input_files/SeaSampler_corrected/' + file_name + '.csv', index=False, sep=',')
+            df.to_csv('../src/input_files/SeaSampler Boreas/' + file_name + '.csv', index=False, sep=',')
             return df, bad_list
         else:
             df = df.loc[df['depth(m)'] >= 1]
-            df.to_csv('../src/input_files/SeaSampler_corrected/' + file_name + '.csv', index=False, sep=',')
+            df.to_csv('../src/input_files/SeaSampler Boreas/' + file_name + '.csv', index=False, sep=',')
             text = 'ERROR DEPTH in: ' + file_name
             bad_list.append(text)
             return df, bad_list
@@ -133,6 +178,8 @@ class SeaSampler():
             bad_list.append('Empty file: ' + file_name)
             return df, bad_list
         tsurface = df_quick['temperature(c)'][0]
+        min_temp = df_quick['temperature(c)'].min()
+        max_temp = df_quick['temperature(c)'].max()
         with open(file_path, newline='') as csvfile:
             # Crear el lector de CSV
             csvreader = csv.reader(csvfile)
@@ -166,8 +213,8 @@ class SeaSampler():
             else:
                 lon = float(bad_lon)
             dur = primeras_lineas[7].split(':', 1)[1][1:]
-            min_temp = primeras_lineas[8].split(':', 1)[1][1:].split(')', 1)[0].split(' ', 1)[0]
-            max_temp = primeras_lineas[8].split(':', 1)[1][1:].split(')', 1)[1].split(' ', 1)[1].split(' ', 1)[0]
+            # min_temp = primeras_lineas[8].split(':', 1)[1][1:].split(')', 1)[0].split(' ', 1)[0]
+            # max_temp = primeras_lineas[8].split(':', 1)[1][1:].split(')', 1)[1].split(' ', 1)[1].split(' ', 1)[0]
             depth = int(-float(primeras_lineas[9].split(':', 1)[1].split(' ')[1]))
             dict = {'User': user, 'Sensor': sensor, 'Dive': dive, 'Date Range': date, 'Latitude': lat, 'Longitude': lon,
                     'Duration': dur, 'Tmin': min_temp, 'Tmax': max_temp, 'Tsurface': tsurface, 'Depth': depth}
@@ -179,11 +226,7 @@ class SeaSampler():
         for file in os.listdir(path):  # use the directory name here
             file_name, file_ext = os.path.splitext(file)
             print(file_name)
-            file_path = path +'/' + file
+            file_path = path + '/' + file
             if file_ext == '.csv':
                 df, bad_list = func(file_path, file_name, df, bad_list)
         return df, bad_list
-
-
-
-
